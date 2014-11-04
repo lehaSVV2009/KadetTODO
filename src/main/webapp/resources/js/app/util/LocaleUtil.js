314bfbabc725c0c96a18673cfd35a75ca8b9ee56
Alex Soroka <AlexSoroka@coherentsolutions.com>
2014-11-04 15:42:08 +0300

    ExtJS Localization

---
+++ src/main/webapp/resources/js/app/util/LocaleUtil.js
@@ -0,0 +1,61 @@
+/**
+ * Created by AlexSoroka on 11/4/2014.
+ */
+Ext.define('kadetTODO.util.LocaleUtil', {
+    signleton: true,
+
+    getWantedLang: function () {
+        return 'en_GB';
+    },
+
+
+    loadProperties: function (text) {
+        var lines = Ext.Array.filter(text.split(/\r\n|\n|\r/), function (line) {
+            return line.trim() !== '' && line.indexOf('#') !== 0;
+        });
+
+        return lines.reduce(function (previousValue, currentValue) {
+            var delimIndex = currentValue.indexOf('='),
+                key = currentValue.substring(0, delimIndex).trim(),
+                value = currentValue.substring(delimIndex + 1, currentValue.length).trim();
+
+            previousValue[key] = value;
+
+            return previousValue;
+        }, []);
+    },
+
+    setLanguage: function (callback) {
+
+        var me = this,
+            preferredLanguage = me.getWantedLang();
+
+        Ext.Ajax.request({
+            url: 'resources/locale/messages_' + preferredLanguage + '.properties',
+            method: 'GET',
+            success: function (response) {
+                var text = response.responseText,
+
+                    properties = me.loadProperties(text),
+
+                    translate = function () {
+                        var key = this.valueOf(),
+                            value = properties[key];
+
+                        if (!value) {
+                            Ext.Msg.alert("Warning", 'Missing translation for: ' + key);
+                            return key;
+                        }
+
+                        return value;
+                    };
+
+                String.prototype.translate = translate;
+
+                callback();
+
+            }
+        });
+
+    }
+});
